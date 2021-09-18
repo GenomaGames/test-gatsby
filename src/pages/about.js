8f0fedf4bc88d7d43cf079b3f541abaafdad512b
Alberto Fernandez <alberto.fernandez@nc43tech.com>
2021-09-18 20:39:25 +0200

    feat(pages, components): add about page and Layout component with some styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,12 +2,12 @@ import * as React from "react";
 
 import Layout from "../components/layout";
 
-const IndexPage = () => {
+const AboutPage = () => {
   return (
-    <Layout pageTitle="Genoma Games">
+    <Layout pageTitle="About Genoma Games">
       <p>Independent indie game development studio based on Madrid, Spain.</p>
     </Layout>
   );
 };
 
-export default IndexPage;
+export default AboutPage;
