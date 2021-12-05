a250bbce8e3d1cbd1951bb2bcc725918950c16f9
Alberto Fernandez <albertofdzm@gmail.com>
2021-12-05 21:21:55 +0100

    ♻️ ass types to {mdx.slug}.tsx
    Signed-off-by: Alberto Fernandez <albertofdzm@gmail.com>

--- src/pages/blog/{mdx.slug}.tsx
+++ src/pages/blog/{mdx.slug}.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
-import { graphql } from "gatsby";
+import { graphql, PageProps } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
-import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { GatsbyImage, getImage, ImageDataLike } from "gatsby-plugin-image";
 
 import Layout from "../../components/layout";
 
 export const query = graphql`
-  query ($id: String) {
+  query BlogPostPage($id: String) {
     mdx(id: { eq: $id }) {
       frontmatter {
         date(formatString: "YYYY-MM-DD")
@@ -24,20 +24,39 @@ export const query = graphql`
   }
 `;
 
-const BlogPost = ({ data }) => {
+type BlogPostPageData = {
+  mdx: {
+    frontmatter: {
+      date: string;
+      title: string;
+      hero_image_alt: string;
+      hero_image_credit_text: string;
+      hero_image_src: ImageDataLike;
+    };
+    body: string;
+  };
+};
+
+type BlogPostPageProps = PageProps<BlogPostPageData>;
+
+const BlogPostPage: React.FunctionComponent<BlogPostPageProps> = ({ data }) => {
   const heroImage = getImage(data.mdx.frontmatter.hero_image_src);
 
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
-      <GatsbyImage
-        image={heroImage}
-        alt={data.mdx.frontmatter.hero_image_alt}
-      />
+      {heroImage ? (
+        <GatsbyImage
+          image={heroImage}
+          alt={data.mdx.frontmatter.hero_image_alt}
+        />
+      ) : (
+        ""
+      )}
       <p>{data.mdx.frontmatter.hero_image_credit_text}</p>
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Layout>
   );
 };
 
-export default BlogPost;
+export default BlogPostPage;
