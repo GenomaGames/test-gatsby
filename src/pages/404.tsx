a3ab931056baffef4fc531bf2dca790228d6e9a1
Alberto Fernandez <albertofdzm@gmail.com>
2021-12-06 00:39:34 +0100

    ♻️ update 404 page to use Layout component
    Signed-off-by: Alberto Fernandez <albertofdzm@gmail.com>

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,22 +1,7 @@
 import * as React from "react";
 import { Link } from "gatsby";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
-
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-
-const paragraphStyles = {
-  marginBottom: 48,
-};
+import Layout from "../components/layout";
 
 const codeStyles = {
   color: "#8A6534",
@@ -26,30 +11,23 @@ const codeStyles = {
   borderRadius: 4,
 };
 
-// markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
+    <Layout pageTitle="Not found">
+      <p>
         Sorry{" "}
         <span role="img" aria-label="Pensive emoji">
           😔
         </span>{" "}
         we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
       </p>
-    </main>
+      {process.env.NODE_ENV === "development" ? (
+        <p>
+          Try creating a page in <code style={codeStyles}>src/pages/</code>.
+        </p>
+      ) : null}
+      <Link to="/">Go home</Link>.
+    </Layout>
   );
 };
 
